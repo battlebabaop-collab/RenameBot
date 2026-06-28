878b1eaac2b02107eb5eae69ab6db90d174c5219
battlebabaop-collab <battlebabaop@gmail.com>
2026-06-28 21:57:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '770434685').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "@plexmovies") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "@Anomovies") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001732233845"))
 
     # wes response configuration     
